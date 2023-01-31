54cfa08f45372e2dbe683c167525e109c2c306e5
FengAnMeng <fenganmeng0326@163.com>
2023-01-31 18:38:05 +0800

    建库

---
+++ Runtime/Entity/EntityLogic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuickGameFramework.Runtime {
+    public class EntityLogic : MonoBehaviour {
+        public void OnInit() {
+            
+        }
+
+        public void OnRecycle() {
+            
+        }
+
+        public void OnUpdate() {
+            
+        }
+    }
+}
